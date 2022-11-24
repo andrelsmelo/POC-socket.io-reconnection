d10d80d716c507dbe928f796070f2e2399922408
André Melo <andrelucassmelo@gmail.com>
2022-11-24 19:51:51 -0300

    first commit

---
+++ build/precache-manifest.8737e7c5a97ccb72d771352f41c0178d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031185c41ba37cc5f697761fbf2d71a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a61f6fd148c825aba30",
+    "url": "/static/css/main.875cf953.chunk.css"
+  },
+  {
+    "revision": "90263f938b744c781a5a",
+    "url": "/static/js/2.07e11fa8.chunk.js"
+  },
+  {
+    "revision": "222b4a5f5cd655e34e2a29e8aabff23f",
+    "url": "/static/js/2.07e11fa8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a61f6fd148c825aba30",
+    "url": "/static/js/main.fcdc6f8b.chunk.js"
+  },
+  {
+    "revision": "7855791e2a868f88edeb",
+    "url": "/static/js/runtime-main.d772115f.js"
+  }
+]);
